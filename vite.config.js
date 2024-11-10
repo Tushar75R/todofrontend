768c10c582b616c9ff8ee59130e1f6531935f4eb
Tushar75R <mylmye07@gmail.com>
2024-11-10 23:55:14 +0530

    first commit

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import vercel from "vite-plugin-vercel";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  server: {
+    port: process.env.PORT,
+  },
+  plugins: [vercel()],
+});
